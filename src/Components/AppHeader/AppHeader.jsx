cdca3ea338be280fdb9bbc7214039cecd5baa83b
bit.bunnary <51nicoleberger@gmail.com>
2025-10-04 20:49:29 -0300

    Added camera button to AppHeader

--- src/Components/AppHeader/AppHeader.jsx
+++ src/Components/AppHeader/AppHeader.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './AppHeader.css'
+import { TbCameraHeart } from "react-icons/tb";
 
 const AppHeader = () => {
     return (
         <div className="app-header">
-            <img src="/LogoSD.png" alt="Logo" className="app-header__logo" />
-            <h1 className="app-header__title">SakuraDreams</h1>
+            <div className='app-header__LnR'>
+                <img src="/LogoSD.png" alt="Logo" className="app-header__logo" />
+                <h1 className="app-header__title">SakuraDreams</h1>
+            </div>
+            <div className='app-header__LnR'>
+                <button className='app-header__button' title='Cámara'><TbCameraHeart /></button>
+            </div>
         </div>
     );
 };
